4ed641c218faa3ac0b0eee789c89a92c5eaeb7b0
okshi <johnstarhafiz@gmail.com>
2021-11-11 20:04:33 +0600

    Clear everythings

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ async function run() {
                res.json(result)
           })
 
-          //Make admin
+          //Make Admin
           app.put('/users/admin', async(req, res)=>{
                const user = req.body;
                const filter = {email : user.email};
