c4f1555087ca9762a47bb23b14ee90bce81ae3e8
okshi <johnstarhafiz@gmail.com>
2021-11-11 19:12:15 +0600

    Server API DONE inmeantime

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 
-//MiddleWare & Body parser
+//MiddleWare & Body Parser
 app.use(cors());
 app.use(express.json());
 
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 
 
-//Uri and Database string
+//Uri and Database String
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.luy9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
